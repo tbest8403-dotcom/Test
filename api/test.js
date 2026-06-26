95ea652b1cde72ba68368ce76f522c35a24f29bc
tbest8403-dotcom <tbest8403@gmail.com>
2026-06-26 02:42:53 -0700

    Update test.js

--- api/test.js
+++ api/test.js
@@ -1,1 +1,8 @@
+export default function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
 
+  return res.status(200).json({
+    success: true,
+    message: "Vercel fetch works!"
+  });
+}
